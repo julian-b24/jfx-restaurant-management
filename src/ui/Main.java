e1e16167ed0a047c8e7fbb6d59bd447f70ef72f5
Julian Bolanos <julian.camilob24@gmail.com>
2021-03-13 23:09:25 -0500

    Main class added in ui package, several amoount of lines are commented cause the controller class is not implemented.

---
+++ src/ui/Main.java
@@ -0,0 +1,39 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.Restaurant;
+
+public class Main extends Application{
+
+	private Restaurant restaurant;
+	//private RestaurantGUI restaurantGUI;
+	
+	public Main() {
+		restaurant = new Restaurant();
+		//restaurantGUI = new RestaurantGUI(restaurant);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("path"));
+		
+		//fxmlLoader.setController(restaurantGUI);
+		Parent root = fxmlLoader.load();
+		
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Casa Dorada");
+		primaryStage.show();
+		//Add method to show the login
+	}
+
+}
