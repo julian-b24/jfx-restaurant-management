926f88f769661a61541767055b4ff7bdd28ee3d3
Julian Bolanos <julian.camilob24@gmail.com>
2021-03-16 22:29:47 -0500

    List of references added to the class Ingredient.

--- src/model/Saleable.java
+++ src/model/Saleable.java
@@ -1,22 +1,20 @@
 package model;
 
+
 public abstract class Saleable {
 	
 	//WARNING CONSTANTS NOT IN THE CLASS DIAGRAM
-	public final static boolean DEAFULT_REFERENCE = false;		//When a saleable is created, it is by deafult not referenced
 	public final static boolean DEAFULT_AVAILABLE = true;		//When a saleable is created, it is by deafult available
 	
 	private String name;			//Name of the saleable
 	private String lastEditorRef;	//Id of the last editor
 	private int code;				//Code of the saleable
-	private boolean productRef;		//Reference of the saleable in any other product, true if is referenced, false in case of it's not referenced
 	private boolean available;		//State of the product, true in case is enable, false in any other way
 	
 	public Saleable(String nm, String lastE, int lastCode) {
 		name = nm;
 		lastEditorRef = lastE;
 		code = lastCode + 1;
-		productRef = DEAFULT_REFERENCE;
 		available = DEAFULT_AVAILABLE;
 	}
 
@@ -35,14 +33,6 @@ public abstract class Saleable {
 
 	public void setLastEditorRef(String lastEditorRef) {
 		this.lastEditorRef = lastEditorRef;
-	}
-
-	public boolean isProductRef() {
-		return productRef;
-	}
-
-	public void setProductRef(boolean productRef) {
-		this.productRef = productRef;
 	}
 
 	public boolean isAvailable() {
