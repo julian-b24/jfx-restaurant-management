9fe5d803e020b653c7fadf484cc8f734be21871e
Julian Bolanos <julian.camilob24@gmail.com>
2021-03-19 14:30:17 -0500

    Enum State modified, the field process was remmoved cause it was unnecessary.

--- src/model/State.java
+++ src/model/State.java
@@ -1,23 +1,18 @@
 package model;
 
 public enum State {
-	REQUESTED ("Solicitado", 0), PROCESS ("En proceso", 1),
-	DELIVERED ("Enviado", 2), RECIEVED ("Entregado", 3);
 	
-	//Unnecessary implementation, check it according to the advance of the program.
+	REQUESTED ("Solicitado"), PROCESS ("En proceso"),
+	DELIVERED ("Enviado"), RECIEVED ("Entregado");
+	
 	private String state;
-	private int process;
 	
-	private State(String stte, int procss) {
+	private State(String stte) {
 		state = stte;
-		process = procss;
 	}
 	
 	public String getState() {
 		return state;
 	}
-	
-	public int getProcess() {
-		return process;
-	}
+
 }
