1b698fad462eab6f5aad991b8643e7bb803aa0ed
Julian Bolanos <julian.camilob24@gmail.com>
2021-03-27 20:29:11 -0500

    Field type added to enum ProductType.

--- src/model/ProductType.java
+++ src/model/ProductType.java
@@ -1,5 +1,16 @@
 package model;
 
 public enum ProductType {
-	MAIN_DISH, DRINK, ADDIOTIONAL_DISH
+	MAIN_DISH ("Plato principal"), DRINK ("Bebida"), ADDIOTIONAL_DISH ("Adición");
+
+	private String type;
+	
+	private ProductType(String typ) {
+		type = typ;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
 }
